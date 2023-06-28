fee5202e824cf422b25fb1b79491cdded87e3f22
NikitochkaHrum <ponkin.nikitka@mail.ru>
2023-06-29 00:39:46 +0500

    All

--- products/tests/conftest.py
+++ products/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from django.core.management import call_command
 from products.models import Product
 
-@pytest.fixture(scope='session')
-def django_db_setup(django_db_setup, django_db_blocker):
+@pytest.fixture(scope='function')
+def init_data(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock(): 
-        call_command('loaddata', "init_data.json")
+        call_command('loaddata', "init_data.json") # данные для заполнения лежат в init_data.json
--- products/tests/test_products.py
+++ products/tests/test_products.py
@@ -1,17 +1,11 @@
-import pytest
 from products.models import Product
+import pytest
 
 @pytest.mark.django_db
 def test_product_creation():
-       product = Product.objects.create(
-           name='T-Shirt',
-           price=19.99,
-           description='Comfortable cotton t-shirt',
-           category='Tops',
-           image='./static/product_images/placeholder.png'
-       )
-       
-       assert product.name == 'T-Shirt'
-       assert product.price == 19.99
-       assert product.description == 'Comfortable cotton t-shirt'
-       assert product.category == 'Tops'
+    product = Product.objects.create(name='New Product', price=9.99, description='Test product', category='Test Category')
+    assert product.id is not None
+    assert product.name == 'New Product'
+    assert product.price == 9.99
+    assert product.description == 'Test product'
+    assert product.category == 'Test Category'
